721553f055806bb03c99ee4993a39a6c344ae2ba
AjloriaCodex <Ajloria123@gmail.com>
2024-08-30 20:00:25 +0800

    changes in refresh, and scrolls

---
+++ script.js
@@ -0,0 +1,9 @@
+
+const img = document.getElementById('visionboard');
+
+img.onload = function(){
+    img.style.display = 'block'; // Show the image
+    img.classList.add('loaded'); // Mark the image as loaded
+};
+
+
